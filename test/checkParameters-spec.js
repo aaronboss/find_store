afcde2909fe81446b5c5a92a15eec0841fbc6f66
Aaron Boss <aboss@targetpharmasolutions.com>
2019-05-30 10:51:15 -0400

    Added tests and split out the functionality for better test coverage.

---
+++ test/checkParameters-spec.js
@@ -0,0 +1,48 @@
+const expect = require("expect.js");
+
+const parameters = require("../src/checkParameters");
+
+describe("parameter functionality", function() {
+  it("get an full data object from check parameters", function() {
+    const data = parameters.checkRequiredParameters([
+      "--address=233 S Wacker Dr, Chicago, IL 60606",
+      "--units=km",
+      "--output=json"
+    ]);
+    console.log("data", data);
+    expect(data).to.be.ok();
+    expect(data).to.be.an("object");
+
+    // expect to get the address
+    expect(data.address).to.be.ok();
+    expect(data.address).to.eql("233 S Wacker Dr, Chicago, IL 60606");
+
+    // expect to get the units
+    expect(data.units).to.be.ok();
+    expect(data.units).to.eql("km");
+
+    // expect to get the output
+    expect(data.output).to.be.ok();
+    expect(data.output).to.eql("json");
+  });
+  it("get an full data object with defaults from check parameters", function() {
+    const data = parameters.checkRequiredParameters([
+      "--address=233 S Wacker Dr, Chicago, IL 60606"
+    ]);
+    console.log("data", data);
+    expect(data).to.be.ok();
+    expect(data).to.be.an("object");
+
+    // expect to get the address
+    expect(data.address).to.be.ok();
+    expect(data.address).to.eql("233 S Wacker Dr, Chicago, IL 60606");
+
+    // expect to get the units
+    expect(data.units).to.be.ok();
+    expect(data.units).to.eql("mi");
+
+    // expect to get the output
+    expect(data.output).to.be.ok();
+    expect(data.output).to.eql("text");
+  });
+});
---
+++ test/location-spec.js
@@ -0,0 +1,114 @@
+const assert = require("assert");
+const expect = require("expect.js");
+
+var location = require("../src/location");
+describe("location functionality", function() {
+  it("get an address from geocode", function() {
+    return location
+      .getCurrentLocation("123 Main Street, New York, NY")
+      .then(function(data) {
+        // verify that data is returned
+        expect(data).to.be.ok();
+
+        // the lat and lng is accurate
+        expect(data.results[0].geometry).to.eql({
+          lat: 42.131738,
+          lng: -73.8906118
+        });
+      });
+  });
+  it("get all csv data", function() {
+    return location.getCSVData().then(function(data) {
+      // verify that there is data returned
+      expect(data).to.be.ok();
+
+      // data is an array
+      expect(data).to.be.an("array");
+
+      // data length is greater than 0
+      expect(data.length).to.be.above(0);
+    });
+  });
+  it("get closest store, distance in miles, json output", async function() {
+    const currentLocation = await location.getCurrentLocation(
+      "123 Main Street, New York, NY"
+    );
+    const csvData = await location.getCSVData();
+    return location
+      .findClosestStore(currentLocation, csvData, "mi", "json")
+      .then(function(data) {
+        // verify json as a param results in an object as a result
+        expect(data).to.be.an("object");
+
+        // verify json data
+        expect(data["Store Name"]).to.eql("Kingston");
+        expect(data["Store Location"]).to.eql("SEC Rte 9W & Rte 209");
+        expect(data.Address).to.eql("1300 Ulster Ave");
+        expect(data.City).to.eql("Kingston");
+        expect(data.State).to.eql("NY");
+        expect(data["Zip Code"]).to.eql("12401-1501");
+        expect(data.Latitude).to.eql("41.966857");
+        expect(data.Longitude).to.eql("-73.984031");
+        expect(data.County).to.eql("Ulster County");
+        expect(data.distance).to.eql("12.37336455102203");
+        expect(data.distanceUnits).to.eql("mi");
+      });
+  });
+  it("get closest store, distance in kilometers, json output", async function() {
+    const currentLocation = await location.getCurrentLocation(
+      "123 Main Street, New York, NY"
+    );
+    const csvData = await location.getCSVData();
+    return location
+      .findClosestStore(currentLocation, csvData, "km", "json")
+      .then(function(data) {
+        // verify data is an object returned
+        expect(data).to.be.an("object");
+
+        // verify data object
+        expect(data["Store Name"]).to.eql("Kingston");
+        expect(data["Store Location"]).to.eql("SEC Rte 9W & Rte 209");
+        expect(data.Address).to.eql("1300 Ulster Ave");
+        expect(data.City).to.eql("Kingston");
+        expect(data.State).to.eql("NY");
+        expect(data["Zip Code"]).to.eql("12401-1501");
+        expect(data.Latitude).to.eql("41.966857");
+        expect(data.Longitude).to.eql("-73.984031");
+        expect(data.County).to.eql("Ulster County");
+        expect(data.distance).to.eql("19.913");
+        expect(data.distanceUnits).to.eql("km");
+      });
+  });
+  it("get closest store, distance in miles, text output", async function() {
+    const currentLocation = await location.getCurrentLocation(
+      "123 Main Street, New York, NY"
+    );
+    const csvData = await location.getCSVData();
+    return location
+      .findClosestStore(currentLocation, csvData, "mi", "text")
+      .then(function(data) {
+        // verify that if text is specified as a param,the output is a string
+        expect(data).to.be.a("string");
+        // verify that the address is the same and the distance is in miles
+        expect(data).to.eql(
+          "1300 Ulster Ave\nKingston, NY 12401-1501\nDistance: 12.37336455102203 mi"
+        );
+      });
+  });
+  it("get closest store, distance in kilometers, text output", async function() {
+    const currentLocation = await location.getCurrentLocation(
+      "123 Main Street, New York, NY"
+    );
+    const csvData = await location.getCSVData();
+    return location
+      .findClosestStore(currentLocation, csvData, "km", "text")
+      .then(function(data) {
+        // verify that if text is specified as a param, the output is a string
+        expect(data).to.be.a("string");
+        // verify that the address is the same and the distance is in kilometers
+        expect(data).to.eql(
+          "1300 Ulster Ave\nKingston, NY 12401-1501\nDistance: 19.913 km"
+        );
+      });
+  });
+});
